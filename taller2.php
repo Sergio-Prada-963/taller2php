2918665dfd19fcedbdde34a5979ac9cf4116c7b6
Sergio Prada <alejanprada140705@gmail.com>
2023-05-18 20:20:16 -0500

    feat: :bug: apenas va el primero funcional
    podria hacer un cosidicional despues del enviar datos que cuando menu sea = a alguna opcion mustre esos resultados

--- taller2.php
+++ taller2.php
@@ -25,19 +25,90 @@
             <option value="objetoForEach">13. Iterar Objetos Con ForEach</option>
             <option value="objetoMap">14. Iterar Objeto con MAP y crear copia</option>
         </select>
-        <button type="submit">Vamos</button>
+        <input type="submit" value="Enviar" name="ejecutar">
     </form>
+    <form method="POST">
+        <?php
+            $menu = $_POST["menu"];
+            $nombre;
+            $edad;
+            switch($menu){
+                case "lectura";
+                    echo "
+                        <h2>Datos Personales</h2>
+                        <label>Nombre</label>
+                        <input type='text' placeholder='Ingrese su nombre' name='nombre'>
+                        <label>Edad</label>
+                        <input type='number' placeholder='Ingrese su edad' name='edad'>";
+                    break;
+            };
+            echo "<input type='submit' value='subir datos' name='envio'class='btn btn-warning m-2' ><br>";
+            $nombre = $_POST["nombre"];
+            $edad = $_POST["edad"];
+            echo "Su nombre es: $nombre y su edad es: $edad";
+        ?>
+    </form>
+
+
+
+    <div class="caja6">
+        <form action="taller2.php" method="post">
+            <label for="contador">Ingrese la cantidad de estudiantes: </label>
+            <input type="number" name="contador">
+            <input type="submit" value="Enviar" class="btn btn-warning m-2">
+        </form>
+        <form action="" method="post">
+            <?php
+                $contador = $_POST["contador"];
+                $nombres = [];
+                $sexos = [];
+                $notas = [];
+                $numeroMujeres = 0;
+                $numeroHombres = 0;
+                for ($i=1; $i <= $contador; $i++) {
+                    echo "
+                    <label>Digitar Nombre {$i}</label>
+                    <input type='text' name='nombre$i' >
+                    <label>Digitar Sexo {$i}</label>
+                    <select name='genero$i'>
+                        <option value=''>Seleccione</option>
+                        <option value='hombre'>hombre</option>
+                        <option value='mujer'>mujer</option>
+                    </select>
+                    <label>Digitar Nota {$i}</label>
+                    <input type='number' name='nota$i' step='0.1'><br><br>";
+                }
+                echo "<input type='hidden' name='contador' value='$contador'>";
+                echo "<input type='submit' value='subir datos' name='envio'class='btn btn-warning m-2' ><br>";
+                for ($i=1; $i <= $contador; $i++) {
+                    $nombre = $_POST["nombre$i"];
+                    $sexo = $_POST["genero$i"];
+                    $nota = $_POST["nota$i"];
+                    $nombres[] = $nombre;
+                    $sexos[] = $sexo;
+                    $notas[] = $nota;
+                
+                    if($sexo){
+                        if($sexo == 'hombre'){
+                            $numeroHombres++;
+                        }else{
+                            $numeroMujeres++;
+                        }
+                    }
+                } 
+
+                $notaMayor = max($notas);
+                $notaMenor = min($notas);
+                $posicionMayor = array_search($notaMayor, $notas);
+                $posicionMenor = array_search($notaMenor, $notas);
+                $estudianteMax = array_values($nombres)[$posicionMayor];
+                $estudianteMin = array_values($nombres)[$posicionMenor];    
+            
+                echo "<br>El estudiante con mayor nota es: $estudianteMax con una nota de $notaMayor";
+                echo "<br>El estudiante con menor nota es: $estudianteMin con una nota de $notaMenor";
+                echo "<br>Hay un total de $numeroHombres hombres y $numeroMujeres mujeres"
+            ?>
+        </form>
+    </div>
 </body>
 </html>
-<?php
-    $menu = $_POST["menu"];
-        if($_POST){
-            if($menu === "lectura"){
-                echo "<label>Ingrese su Nombre</label>
-                <input type='text' placeholder='Nombre'>
-                <label>Ingrese su edad</label>
-                <input type='number' placeholder='Ingrese su Edad'>
-                <button type='submit'>Enviar</button>";
-            };
-        }
-?>
