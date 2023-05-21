caa157a09b22860234295265393ba2b5ef4b5e7f
Sergio Prada <alejanprada140705@gmail.com>
2023-05-21 16:38:16 -0500

    feat: :poop: Error
    no funciona el mostrar datos, no se selecciona el input de submit

--- taller2.php
+++ taller2.php
@@ -24,110 +24,139 @@
             <option value="objetoFor">12. Iterar Objetos Con FOR</option>
             <option value="objetoForEach">13. Iterar Objetos Con ForEach</option>
             <option value="objetoMap">14. Iterar Objeto con MAP y crear copia</option>
+            <input type="submit" value="Enviar" name="submit">
         </select>
-        <input type="submit" value="Enviar" name="ejecutar">
     </form>
-    <form method="POST">
+    <!-- <form method="POST"> -->
         <?php
-            $menu = $_POST["menu"];
-            echo $menu;
-            $nombre;
-            $edad;
-            switch($menu){
-                case "lectura";
-                    echo "
-                        <h2>Datos Personales</h2>
-                        <label>Nombre</label>
-                        <input type='text' placeholder='Ingrese su nombre' name='nombre'>
+            session_start();
+            if(isset($_POST['submit'])){
+                $menu = $_POST['menu'];
+                switch($menu){
+                    case 'lectura';
+                        echo "
+                        <form action='taller2.php' method='POST'>
+                            <h2>Datos Personales</h2>
+                            <label>Nombre</label>
+                            <input type='text' placeholder='Ingrese su nombre' name='nombre'>
+                            <label>Edad</label>
+                            <input type='number' placeholder='Ingrese su edad' name='edad'>
+                            <input type='submit' value='subir datos' name='verr'> <br>
+                        </form>";
+                            if(($_POST['verr'])){
+                                $nombre = $_POST['nombre'];
+                                $_SESSION['nombre'] = $nombre;
+                                echo $_SESSION['nombre'];
+                                $edad = $_POST['edad'];
+                                $_SESSION['edad'] = $edad;
+                                echo $_SESSION['nombre'];
+                                echo "Su nombre es: $nombre y su edad es: $edad";   
+                            }else{ echo "no funcione :(";}
+                        break;
+                    case "crearO";
+                        echo "<h2>Creando Objeto...</h2>
+                        <label>Nombre de tu mascota</label>
+                        <input type='text' placeholder='Ingrese el nombre de la mascota' name='nombreMascota'>
+                        <label>Tipo de Mascota</label>
+                        <input type='text' placeholder='Ingrese el tipo de mascota' name='tipoMascota'>
                         <label>Edad</label>
-                        <input type='number' placeholder='Ingrese su edad' name='edad'>";
-                    break;
-                case "crearO";
-                    echo "<h2>Creando Objeto...</h2>
-                    <label>Nombre de tu mascota</label>
-                    <input type='text' placeholder='Ingrese el nombre de la mascota' name='nombreMascota'>
-                    <label>Tipo de Mascota</label>
-                    <input type='text' placeholder='Ingrese el tipo de mascota' name='tipoMascota'>
-                    <label>Edad</label>
-                    <input type='text' placeholder='Ingrese la edad de la mascota' name='edadMascota'>
-                    <label>Color</label>
-                    <input type='text' placeholder='Ingrese el color de su mascota' name='colorMascota'>";
-                    break;
-            };
-            echo "<input type='submit' value='subir datos' name='envio'class='btn btn-warning m-2' ><br>";
-            /* punto #1 */
-                $nombre = $_POST["nombre"];
-                $edad = $_POST["edad"];
-                echo "Su nombre es: $nombre y su edad es: $edad";
-            /* punto #2 */
-                $tipoMasocota = $_POST["tipoMascota"];
-                $nombreMasocota = $_POST["nombreMascota"];
-                $edadMasocota = $_POST["edadMascota"];
-                $colorMasocota = $_POST["colorMascota"];
-                class Objetos 
-                {
-                    private $mascota;
-                    private $nombre;
-                    private $edad;
-                    private $color;
+                        <input type='number' placeholder='Ingrese la edad de la mascota' name='edadMascota'>
+                        <label>Color</label>
+                        <input type='text' placeholder='Ingrese el color de su mascota' name='colorMascota'>";
+                        break;
+                };
+                
+                    if($menu == "lectura"){
+                        echo "Soy la lectura";
+                        if(($_POST)){
+                            echo "fuy enviadooo";
+                            /* punto #1 */
+                            $nombre = $_POST["nombre"];
+                            $_SESSION['nombre'] = $nombre;
+                            echo $_SESSION['nombre'];
+                            $edad = $_POST["edad"];
+                            $_SESSION['edad'] = $edad;
+                            echo $_SESSION['nombre'];
+                            echo "Su nombre es: $nombre y su edad es: $edad";
+                        }
+                        $enviado = $_POST["envio"];
+                        if($enviado){
+                        echo "fuy enviadooo yo en el botoooon";
+                        }
+                    }
                     
-                    public function __construct($mascota, $nombre, $edad, $color){
-                        $this->mascota = $mascota;
-                        $this->nombre = $nombre;
-                        $this->edad = $edad;
-                        $this->color = $color;
+                /* punto #2 */
+                    $tipoMasocota = $_POST["tipoMascota"];
+                    $nombreMasocota = $_POST["nombreMascota"];
+                    $edadMasocota = $_POST["edadMascota"];
+                    $colorMasocota = $_POST["colorMascota"];
+                    class Objetos 
+                    {
+                        private $mascota;
+                        private $nombre;
+                        private $edad;
+                        private $color;
+                        
+                        public function __construct($mascota, $nombre, $edad, $color){
+                            $this->mascota = $mascota;
+                            $this->nombre = $nombre;
+                            $this->edad = $edad;
+                            $this->color = $color;
+                        }
+    
+                        public function getMascota(){
+                            return $this->mascota;
+                        }
+                        public function getNombre(){
+                            return $this->nombre;
+                        }
+                        public function getEdad(){
+                            return $this->edad;
+                        }
+                        public function getColor(){
+                            return $this->color;
+                        }
                     }
-
-                    public function getMascota(){
-                        return $this->mascota;
+                /* Punto #3 */
+                    if($menu == "mostrarO"){
+                        $pet = new Objetos($tipoMasocota, $nombreMasocota, $edadMasocota, $colorMasocota);
+                        echo "<h3>Tipo de Mascota: </h3>";
+                        echo $pet->getMascota() . "<br>" . "<h3>Nombre de la Mascota: </h3>";
+                        echo $pet->getNombre() . "<br>" . "<h3>Edad de la Mascota: </h3>";
+                        echo $pet->getEdad() . "<br>" . "<h3>Color de la Mascota: </h3>";
+                        echo $pet->getColor();
                     }
-                    public function getNombre(){
-                        return $this->nombre;
-                    }
-                    public function getEdad(){
-                        return $this->edad;
-                    }
-                    public function getColor(){
-                        return $this->color;
-                    }
-                }
-                $pet = new Objetos($tipoMasocota, $nombreMasocota, $edadMasocota, $colorMasocota);
-                echo "<h3>Tipo de Mascota: </h3>";
-                echo $pet->getMascota() . "<br>" . "<h3>Nombre de la Mascota: </h3>";
-                echo $pet->getNombre() . "<br>" . "<h3>Edad de la Mascota: </h3>";
-                echo $pet->getEdad() . "<br>" . "<h3>Color de la Mascota: </h3>";
-                echo $pet->getColor();
-
+            }
         ?>
-    </form>
+    <!-- </form> -->
 
 <?php
-class MiClase
-{
-    public $var1 = 'valor 1';
-    public $var2 = 'valor 2';
-    public $var3 = 'valor 3';
+// class MiClase
+// {
+//     public $var1 = 'valor 1';
+//     public $var2 = 'valor 2';
+//     public $var3 = 'valor 3';
 
-    protected $protected = 'variable protegida';
-    private   $private   = 'variable privada';
+//     protected $protected = 'variable protegida';
+//     private   $private   = 'variable privada';
 
-    function iterateVisible() {
-       echo "MiClase::iterateVisible:\n";
-       foreach ($this as $clave => $valor) {
-           print "$clave => $valor\n";
-       }
-    }
-}
+//     function iterateVisible() {
+//        echo "MiClase::iterateVisible:\n";
+//        foreach ($this as $clave => $valor) {
+//            print "$clave => $valor\n";
+//        }
+//     }
+// }
 
-$clase = new MiClase();
+// $clase = new MiClase();
 
-foreach($clase as $clave => $valor) {
-    print "$clave => $valor\n";
-}
-echo "\n";
+// foreach($clase as $clave => $valor) {
+//     print "$clave => $valor\n";
+// }
+// echo "\n";
 
 
-$clase->iterateVisible();
+// $clase->iterateVisible();
 
 ?>
 
